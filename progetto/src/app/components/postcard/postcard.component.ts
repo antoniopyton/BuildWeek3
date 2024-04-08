dfefe9853ee746de99cf2469db428c1f19f10e3d
Faffo96 <fabioscar96@gmail.com>
2024-04-08 17:08:10 +0200

    postcard implements

--- progetto/src/app/components/postcard/postcard.component.ts
+++ progetto/src/app/components/postcard/postcard.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Post } from 'src/app/models/post.interface';
+import { Input } from '@angular/core';
+import { PostService } from 'src/app/service/post.service';
 
 @Component({
   selector: 'app-postcard',
@@ -6,5 +9,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./postcard.component.scss']
 })
 export class PostcardComponent {
+  @Input() post!: Post;
+  mostra = true;
 
+  constructor(private postSrv: PostService) { }
+
+  deletePost(id: number) {
+    this.postSrv.deletePost(id).subscribe();
+    this.mostra = false;
+  }
 }
