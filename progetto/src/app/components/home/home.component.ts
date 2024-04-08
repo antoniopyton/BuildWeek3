ce61b84af020d23e916168844aa28ad6fc7c5319
antoniopyton <acopetti99@gmail.com>
2024-04-08 15:21:11 +0200

    Modifiche

--- progetto/src/app/components/home/home.component.ts
+++ progetto/src/app/components/home/home.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostService } from 'src/app/service/post.service';
+import { Post } from 'src/app/models/post.interface';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  posts: Post[] = [];
 
+  constructor(private PostsSrv: PostService) {}
+
+  ngOnInit(): void {}
+
+  getPosts() {
+    this.PostsSrv.getPosts().subscribe((data) => {
+      this.posts = data;
+    });
+  }
 }
