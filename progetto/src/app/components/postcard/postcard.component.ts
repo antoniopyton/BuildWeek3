a0f6b027a70d7477c7d4e0752dab29609c0f9e8d
Faffo96 <fabioscar96@gmail.com>
2024-04-09 16:53:49 +0200

    Co-authored-by: GabScognamiglio <GabScognamiglio@users.noreply.github.com> Co-authored-by: antoniopyton <antoniopyton@users.noreply.github.com> Co-authored-by: kevinloffredi <Kevin-Lof@users.noreply.github.com> Co-authored-by: Massimiliano Dalla Mura <MassimilianoDallaMura@users.noreply.github.com>

--- progetto/src/app/components/postcard/postcard.component.ts
+++ progetto/src/app/components/postcard/postcard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Post } from 'src/app/models/post.interface';
 import { Input } from '@angular/core';
 import { PostService } from 'src/app/service/post.service';
@@ -6,6 +6,8 @@ import { FormsModule, NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/auth/auth.service';
 import { AuthData } from 'src/app/models/auth-data.interface';
+import { LikeService } from 'src/app/service/like.service';
+import { Like } from 'src/app/models/like.interface';
 
 
 @Component({
@@ -15,29 +17,72 @@ import { AuthData } from 'src/app/models/auth-data.interface';
 })
 export class PostcardComponent implements OnInit {
   @Input() post!: Post;
-  user!: AuthData | null 
-  mostra = true;
+  user!: AuthData | null
+  show = true;
+  likeCount = 0;
+  likes:Like[]=[];
+  userLikes:Like[]=[];
+  toggle!: boolean;
 
-  constructor(private postSrv: PostService, private router:Router, private authsrv: AuthService) { }
+  constructor(private postSrv: PostService, private router: Router, private authsrv: AuthService, private likeSrv: LikeService) { }
 
   ngOnInit(): void {
     this.authsrv.user$.subscribe((data) => {
-      this.user = data
+      this.user = data;
     })
+    this.getLikes();
   }
 
   deletePost(id: number) {
     this.postSrv.deletePost(id).subscribe();
-    this.mostra = false;
+    this.show = false;
   }
 
-/*   editTitle=this.post.title;
-  editBody=this.post.body */
+  getLikes() {
+    this.likeSrv.getLikes().subscribe((data)=>{
+      this.likes=data;
+      const activeUser: any = localStorage.getItem('user')
+      const activeUserId = JSON.parse(activeUser).user.id;
+      this.userLikes=this.likes.filter((fav)=>fav.userId===activeUserId)
+
+    });
+    this.show = true;
+  }
+
+  addLike(likedPostId: number) {
+    const activeUser: any = localStorage.getItem('user')
+    const activeUserId = JSON.parse(activeUser).user.id;
+    const like = {
+      postId: likedPostId,
+      userId: activeUserId
+    }
+    this.likeSrv.createLike(like).subscribe();
+    this.isFav(likedPostId)
+    this.getLikes();
+  }
+
+  removeLike(postId: number) {
+    let daRimuovere:any = this.userLikes.find(fav=>fav.postId===postId)
+    this.likeSrv.deleteLike(daRimuovere.id).subscribe();
+    this.likes = [];
+      this.isFav(postId)
+  }
+  getLikeCount() {
+    this.postSrv.getPostLikes(this.post.id).subscribe((count) => this.likeCount = count)
+  }
+  /*   editTitle=this.post.title;
+    editBody=this.post.body */
 
   editPost(form: NgForm) {
     console.log(form.value);
     this.postSrv.editPost(this.post.id, form.value).subscribe();
     location.reload()
-    
+  }
+
+  isFav(id: number) {
+    if (this.likes.find(fav => fav.postId === id)) {
+      return true
+    }
+    else { return false }
   }
 }
---
+++ progetto/src/app/models/like.interface.ts
@@ -0,0 +1,5 @@
+export interface Like {
+    userId: number,
+    postId: number,
+    id: number
+}
